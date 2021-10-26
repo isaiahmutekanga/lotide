9aae434bdf673b05e8f7b176af23853acfe0b704
Isaiah Mutekanga <isaiahmutekanga@trentu.ca>
2021-10-26 22:07:19 +0000

    Function to check truthiness

---
+++ eqArrays.js
@@ -0,0 +1,18 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+function eqArrays(arr1, arr2) {
+  if (arr1 == arr2.toString()) {
+    return true;
+  } else return false;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); //false
