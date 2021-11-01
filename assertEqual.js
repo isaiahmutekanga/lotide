f5f313bbce0f956dc2f2ec27879ede04b9c54abd
Isaiah Mutekanga <isaiahmutekanga@trentu.ca>
2021-11-01 14:38:49 +0000

    refractoring

--- assertEqual.js
+++ assertEqual.js
@@ -5,6 +5,4 @@ const assertEqual = function (actual, expected) {
   } else console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
